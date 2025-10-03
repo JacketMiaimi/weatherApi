bd0d106eb848a996d4cf07e10f685e82c709ef85
Marl <vladdudukin006@gmail.com>
2025-10-03 17:42:47 +0300

    добавление логики api, а также логики redis

---
+++ internal/api/getApi.go
@@ -0,0 +1,42 @@
+package getApi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const (
+	api = "23a923037adc48fca74203916252709"
+	baseURL = "https://api.weatherapi.com/v1"
+
+)
+
+type WeatherResp struct {
+	Location struct {
+		Name string `json:"name"`
+	} `json:"location"`
+	Current struct {
+		TempC float64 `json:"temp_c"`
+	} `json:"current"`
+}
+
+
+func WeatherHandler(city string) (*WeatherResp, error) {
+	op := "internal/api/getApi.go"
+	
+	url := fmt.Sprintf("%s/current.json?key=%s&q=%s",baseURL, api, city)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("%s\ncouldn`t get data from api: %w",op, err)
+	} 
+	defer resp.Body.Close()
+
+	var data WeatherResp
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, fmt.Errorf("%s\ncouldn`t decode body: %w", op, err)
+	}
+
+	return &data, nil
+}
