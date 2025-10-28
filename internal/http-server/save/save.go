b323b9988645d30d4771b6f5d4f9095adad5a7f8
Marl <vladdudukin006@gmail.com>
2025-10-28 16:50:52 +0300

    mvp save handler

--- internal/http-server/save/save.go
+++ internal/http-server/save/save.go
@@ -1,6 +1,58 @@
 package save
 
-func New(key string, temp string) (string, int, error) {
-	const op = "handlers.save.save.New"
+import (
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
 
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+
+	resp "go.mod/internal/lib/api/response"
+	"go.mod/internal/lib/logger/sl"
+	red "go.mod/internal/redis"
+)
+
+type Request struct {
+	City string `json:"city"`
+	// Temp string `json:"temp"`
+}
+
+type Response struct {
+	resp.Response
+	Temp string `json:"temp"`
+}
+
+func New(log *slog.Logger, key string, temp string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.save.save.New"
+
+		log := log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if errors.Is(err, io.EOF) {
+			log.Error("request body is empty")
+			render.JSON(w, r, resp.Error("empty request"))
+			return
+		}
+
+		if err != nil {
+			log.Error("failed to decode request body", "error:",sl.Err(err))
+			render.JSON(w,r,resp.Error("failed to decode request"))
+			return 
+		}
+
+		err = red.SaveKey(key,temp)
+		if err != nil {
+			slog.Error("error to save key","error:",err)
+		}
+
+		
+	}
 }
